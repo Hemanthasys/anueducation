f891bf460bd0a36d6c62082b37467f0fd8b0b488
Hemantha <hemantha82@gmail.com>
2026-05-12 10:06:36 +0530

    Add role seeder, super admin seeder, divisional_director role

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,7 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            RoleSeeder::class,
             DivisionSeeder::class,
+            SuperAdminSeeder::class,
         ]);
     }
 }
